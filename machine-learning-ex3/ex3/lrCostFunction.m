72cf5a57d672792fabdb8aa45edbc2e4a95ede02
asmishra <asmishra@users.noreply.github.com>
2017-05-20 02:10:44 -0700

    Add files via upload

---
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -0,0 +1,62 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+
+%Step 1 : Vectorized hypothesis calculation
+h = sigmoid(X*theta); 
+
+%Step 2 : Calculate cost based on hyothesis vs Y (for logistic regression/classification)
+J = (sum(-y.*log(h) - (1-y).*log(1-h)))/m; %some vector math to calculate it in one go
+
+%Step 3 : Generate regularization parameter
+reg = lambda / (2 * m) * (theta' * theta - theta(1)^2); %square and sum all theta values by theta'*theta and remove theta(0)^2 from the sum
+
+%Step 4 : Regularized cost function
+J = J + reg;
+
+%Step 5 : Regularized gradient calculation (vectorized)
+theta_temp = theta;
+theta_temp(1) = 0;
+
+grad = 1 / m * X' * (h - y) + lambda / m * (theta_temp);
+
+% =============================================================
+
+grad = grad(:);
+
+end
